1cfbf4efffd3cf5804fa69b6a523ccef8d143896
nayazjh444 <nayazjh444@gmail.com>
2020-07-01 17:25:31 +0530

    Added jpa-practice project

---
+++ src/main/java/com/nayaz/springboot/jpa/jpapractice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nayaz.springboot.jpa.jpapractice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JpaPracticeApplication.class);
+	}
+
+}
---
+++ src/test/java/com/nayaz/springboot/jpa/jpapractice/JpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayaz.springboot.jpa.jpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
